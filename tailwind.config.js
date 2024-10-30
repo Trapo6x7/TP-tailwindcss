6f897a0e47a7fc9f8f94982ef571a41da30050ac
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-10-30 15:08:36 +0100

    ADD:PLEIN DE CHOSES

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,23 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
-      layout:{
-        mobile: "375px",
-        desktop: "1440px",
-      }
-      color: {
+      colors: {
         primary: {
           orange: "hsl(35, 77%, 62%)",
           red: "hsl(5, 85%, 63%)",
         },
         neutral: {
-          "Off-white": "hsl(36, 100%, 99%)",
-          "Grayish-blue": "hsl(233, 8%, 79%)",
-          "Dark-grayish-blue": "hsl(236, 13%, 42%)",
-          "Very-dark-blue": "hsl(240, 100%, 5%)",
+          "off-white": "hsl(36, 100%, 99%)",
+          "grayish-blue": "hsl(233, 8%, 79%)",
+          "dark-grayish-blue": "hsl(236, 13%, 42%)",
+          "very-dark-blue": "hsl(240, 100%, 5%)",
         },
-      typography: {
-          bodycopy: {
-            "Font-size": "(paragraph): 15px",
-          },
-          font: {
-            Family: "[Inter](https://fonts.google.com/specimen/Inter)",
-            Weights: "400, 700, 800",
-          },
-        },
+      },
+      fontFamily: {
+        body: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        paragraph: "20px",
       },
     },
   },
